439ab53968131377b8f85fe9f1dfab2ee711f2fc
ixc7 <isaacxpreston7@gmail.com>
2022-02-18 07:46:49 -0500

    add to registry

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
+[![clone-all-repos](https://img.shields.io/npm/v/clone-all-repos?style=flat-square)](https://www.npmjs.com/package/clone-all-repos)
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg?style=flat-square)](https://standardjs.com)
 
-### clone-all-repos
+### git-all, clone-all
 
-clones all repos from a github user
-
+Clones all public repositories from a GitHub user
 
 ---
 
-install:
+Install:  
+
+```
+npm i -g clone-all-repos
+git-all|clone-all [username]
+```
+
+or  
+
+```
+npx clone-all-repos [username]
+```
+
+or  
 
 ```
 git clone https://github.com/ixc7/clone-all-repos.git
 cd clone-all-repos
-node index.js <username>
+node index.js [username]
+```
+
+---
+
+Options:  
+
+```
+--help, -h: print this text and exit
+--version, -v: print version and exit (0.0.2)
 ```
--- index.js
+++ index.js
@@ -5,13 +5,15 @@ import { spawn } from 'child_process'
 import { createInterface } from 'readline'
 
 const help = `
-  \rget-all-repos: clones all repos from a github user
+  \rgit-all, clone-all: clones all public repositories from a GitHub user
 
   \rusage:
-  \r       \x1b[1m\x1b[38;5;32mget-all-repos\x1b[0m [expression]
+  \r       \x1b[1m\x1b[38;5;32mgit-all\x1b[0m|\x1b[1m\x1b[38;5;32mclone-all\x1b[0m [username]
 
   \roptions:
-  \r       --help, -h: prints this message
+  \r       --help, -h: print this text and exit
+  \r       --version, -v: print version and exit (0.0.2)
+  
 `
 
 const username = process.argv.slice(2).join('')
@@ -20,15 +22,21 @@ if (username.includes('--help') || username.includes('-h') || !username.length) 
   let code = 0
 
   if (!username.length) {
-    console.log('\n\x1b[1m\x1b[38;5;88merror:\x1b[0m no username specified')
+    console.log('\n\x1b[1m\x1b[38;5;88mError:\x1b[0m no username specified')
     code = 1
   }
 
   console.log(help)
-  process.exit(1)
+  process.exit(code)
 }
 
-const getUserInput = (msg = 'clone?', streamline = false) => {
+if (username.includes('--version') || username.includes('-v')) {
+  console.log('0.0.2')
+  process.exit(0)
+}
+
+const getUserInput = (msg = 'Clone all repos?') => {
+  // streamline: bool (default = false)
   const responses = 'Yesyes'
 
   const rl = createInterface({
@@ -38,13 +46,13 @@ const getUserInput = (msg = 'clone?', streamline = false) => {
   })
 
   return new Promise((resolve, reject) => {
-    if (streamline) resolve(true)
-    
+    // if (streamline) resolve(true)
     rl.prompt()
+
     rl.on('line', line => {
       if (responses.includes(line)) resolve(true)
       else {
-        console.log('cancelled')
+        console.log('Cancelled')
         reject(process.exit(0))
       }
     })
@@ -52,8 +60,8 @@ const getUserInput = (msg = 'clone?', streamline = false) => {
 }
 
 const init = async (user) => {
-  console.log(`fetching repos for user ${user}`)
-  
+  console.log(`Fetching repos for user ${user}`)
+
   get({
     host: 'api.github.com',
     path: `/users/${user}/repos`,
@@ -71,13 +79,13 @@ const init = async (user) => {
         let remaining = len
 
         if (!len) {
-          let msg = 'user not found'
-          if (len === 0) msg = 'user has no public repos'
+          let msg = 'User not found'
+          if (len === 0) msg = 'User has no public repos'
           console.log(msg)
           process.exit(1)
         }
 
-        await getUserInput(`clone \x1b[1m\x1b[38;5;49m${len}\x1b[0m repos?`)
+        await getUserInput(`Clone \x1b[1m\x1b[38;5;49m${len}\x1b[0m repos?`)
 
         for (let i = 0; i < len; i += 1) {
           const { html_url, name } = result[i]
@@ -89,7 +97,7 @@ const init = async (user) => {
               remaining -= 1
 
               if (code === 0) {
-                console.log(`cloned '${name}' (${remaining} left)`)
+                console.log(`Cloned '${name}' (${remaining} left)`)
               } else if (code === 128) {
                 console.log(`${user}/${name} already exists`)
               } else {
@@ -97,15 +105,13 @@ const init = async (user) => {
               }
 
               if (remaining < 1) {
-                console.log('complete')
+                console.log('Complete')
                 process.exit(0)
               }
             })
-
-          } catch (e) { console.log(`error running git clone on ${html_url}:\n`, e.message) }
+          } catch (e) { console.log(`Error running git clone on ${html_url}:\n`, e.message) }
         }
-        
-      } catch (e) { console.log('error fetching data from API:\n', e.message) }
+      } catch (e) { console.log('Error fetching data from API:\n', e.message) }
     })
   })
 }
